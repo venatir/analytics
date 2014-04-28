25869d66500884311db39c2aba3b9edb206d9adc
Mircea Danila Dumitrescu <mircea@shopcade.com>
2014-04-28 01:13:26 +0100

    Changed the way we deal with events and the way we save them, removed most of cube. kept the collector. Added the aggregator

--- bin/collector.js
+++ bin/collector.js
@@ -1,9 +1,9 @@
-var options = require("./collector-config"),
-    cube = require("../"),
-    server = cube.server(options);
+var options = require("./collector-config");
+var cube = require("../");
+var server = cube.server(options);
 
-server.register = function(db, endpoints) {
-  cube.collector.register(db, endpoints);
+server.register = function (db, endpoints) {
+    cube.collector.register(db, endpoints);
 };
 
 server.start();
