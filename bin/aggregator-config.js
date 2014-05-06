abe8105beb07c91c277aab8acce5a5ca533932cc
Mircea Danila Dumitrescu <mircea@shopcade.com>
2014-05-06 16:08:28 +0100

    Finished the aggregation part

--- bin/aggregator-config.js
+++ bin/aggregator-config.js
@@ -18,10 +18,10 @@ module.exports = {
     "aggregations": [
         {
             "reagragatable": true,
-            "impressions": [
+            "t1": [
                 {
                     "$match": {
-                        "type": "impressions",
+                        "type": "t1",
                         "t": "This gets overwritten with the correct expressions for the 1 or 5 minute intervals"
                     }},
                 {
@@ -82,74 +82,74 @@ module.exports = {
                     }
                 }
             ]
-        },
-        {
-            "reagragatable": true,
-            "wants": [
-                {
-                    "$match": {
-                        "type": "wants",
-                        "t": "This gets overwritten with the correct expressions for the 1 or 5 minute intervals"
-                    }},
-                {
-                    "$project": {
-                        _id: 0,
-                        type: "$type",
-                        v1: "$d.v1",
-                        v2: "$d.v2",
-                        v3: {
-                            $cond: [
-                                {
-                                    $eq: ["$d.v3", "US"]
-                                },
-                                "US",
-                                {
-                                    $cond: [
-                                        {
-                                            $eq: ["$d.v3", "GB"]
-                                        },
-                                        "GB",
-                                        {
-                                            $cond: [
-                                                {
-                                                    $eq: ["$d.v3", "JP"]
-                                                },
-                                                "JP",
-                                                {
-                                                    $cond: [
-                                                        {
-                                                            $eq: ["$d.v3", "IN"]
-                                                        },
-                                                        "IN",
-                                                        "OTHER"
-                                                    ]
-                                                }
-                                            ]
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        v4: "$d.v4"
-                    }
-                },
-                {
-                    "$group": {
-                        "_id": {
-                            "type": "$type",
-                            "v1": "$v1",
-                            "v2": "$v2",
-                            "v3": "$v3"
-                        },
-                        "count": {
-                            $sum: 1
-                        },
-                        "index": {
-                            $avg: "$v4"
-                        }
-                    }
-                }
-            ]
         }
+//        ,
+//        {
+//            "reagragatable": true,
+//            "t2": [
+//                {
+//                    "$match": {
+//                        "type": "t2",
+//                        "t": "This gets overwritten with the correct expressions for the 1 or 5 minute intervals"
+//                    }},
+//                {
+//                    "$project": {
+//                        _id: 0,
+//                        type: "$type",
+//                        v1: "$d.v1",
+//                        v2: "$d.v2",
+//                        v3: {
+//                            $cond: [
+//                                {
+//                                    $eq: ["$d.v3", "US"]
+//                                },
+//                                "US",
+//                                {
+//                                    $cond: [
+//                                        {
+//                                            $eq: ["$d.v3", "GB"]
+//                                        },
+//                                        "GB",
+//                                        {
+//                                            $cond: [
+//                                                {
+//                                                    $eq: ["$d.v3", "JP"]
+//                                                },
+//                                                "JP",
+//                                                {
+//                                                    $cond: [
+//                                                        {
+//                                                            $eq: ["$d.v3", "IN"]
+//                                                        },
+//                                                        "IN",
+//                                                        "OTHER"
+//                                                    ]
+//                                                }
+//                                            ]
+//                                        }
+//                                    ]
+//                                }
+//                            ]
+//                        }
+//                    }
+//                },
+//                {
+//                    "$group": {
+//                        "_id": {
+//                            "type": "$type",
+//                            "v1": "$v1",
+//                            "v2": "$v2",
+//                            "v3": "$v3"
+//                        },
+//                        "count": {
+//                            $sum: 1
+//                        },
+//                        "index": {
+//                            $avg: "$index"
+//                        }
+//                    }
+//                }
+//            ]
+//        }
     ]
 };
