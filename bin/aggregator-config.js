49a570d30785844266727dfcd573d700a05ecee5
tikazyq <tikazyq@gmail.com>
2014-09-03 09:59:20 +0100

    added mobile banner impressions

--- bin/aggregator-config.js
+++ bin/aggregator-config.js
@@ -28,6 +28,35 @@ module.exports = {
                     }
                 }
             ]
+        },
+        {
+            "reaggragatable": true,
+            "mobile_banners_impressions": [
+                {
+                    "$match": {
+                        "type": "api.trending.lists" //mandatory
+                    }},
+                {
+                    "$project": {
+                        _id: 0,
+                        type: "$type",
+                        v1: "$d.pagetype",
+                        v2: "$d.u_gdr"
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": {
+                            "type": "$type",
+                            "v1": "$v1",
+                            "v2": "$v2"
+                        },
+                        "count": {
+                            $sum: 1
+                        }
+                    }
+                }
+            ]
         }
         //        ,
         //        {
